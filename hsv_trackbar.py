5905d65032c10ff3969b83e6d6c007191241de1f
ghdus819 <ghdus819@gmail.com>
2021-08-07 18:45:49 +0900

    Add Trackbar Image

--- hsv_trackbar.py
+++ hsv_trackbar.py
@@ -1,7 +1,7 @@
 import sys
 import cv2
 
-src = cv2.imread('beltImage/starbucksBox.png')
+src = cv2.imread('trackbar_image/clip.png')
 
 if src is None:
     print('Image load failed!')
@@ -12,29 +12,31 @@ src_hsv = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
 
 
 def on_trackbar(pos):
-    hmin = cv2.getTrackbarPos('H_min', 'dst')
-    hmax = cv2.getTrackbarPos('H_max', 'dst')
+    hmin = cv2.getTrackbarPos('H_min', 'Trackbar')
+    hmax = cv2.getTrackbarPos('H_max', 'Trackbar')
 
-    smin = cv2.getTrackbarPos('S_min', 'dst')
-    smax = cv2.getTrackbarPos('S_max', 'dst')
+    smin = cv2.getTrackbarPos('S_min', 'Trackbar')
+    smax = cv2.getTrackbarPos('S_max', 'Trackbar')
 
-    vmin = cv2.getTrackbarPos('V_min', 'dst')
-    vmax = cv2.getTrackbarPos('V_max', 'dst')
+    vmin = cv2.getTrackbarPos('V_min', 'Trackbar')
+    vmax = cv2.getTrackbarPos('V_max', 'Trackbar')
 
     dst = cv2.inRange(src_hsv, (hmin, smin, vmin), (hmax, smax, vmax))
-    cv2.imshow('dst', dst)
+    dst = cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR)
+    color_dst = cv2.bitwise_and(src, dst)
+    cv2.imshow('Trackbar', color_dst)
 
 
-cv2.namedWindow('dst')
+cv2.namedWindow('Trackbar')
 
-cv2.createTrackbar('H_min', 'dst', 0, 255, on_trackbar)
-cv2.createTrackbar('H_max', 'dst', 0, 255, on_trackbar)
+cv2.createTrackbar('H_min', 'Trackbar', 0, 255, on_trackbar)
+cv2.createTrackbar('H_max', 'Trackbar', 0, 255, on_trackbar)
 
-cv2.createTrackbar('S_min', 'dst', 0, 255, on_trackbar)
-cv2.createTrackbar('S_max', 'dst', 0, 255, on_trackbar)
+cv2.createTrackbar('S_min', 'Trackbar', 0, 255, on_trackbar)
+cv2.createTrackbar('S_max', 'Trackbar', 0, 255, on_trackbar)
 
-cv2.createTrackbar('V_min', 'dst', 0, 255, on_trackbar)
-cv2.createTrackbar('V_max', 'dst', 0, 255, on_trackbar)
+cv2.createTrackbar('V_min', 'Trackbar', 0, 255, on_trackbar)
+cv2.createTrackbar('V_max', 'Trackbar', 0, 255, on_trackbar)
 
 on_trackbar(0)
 
